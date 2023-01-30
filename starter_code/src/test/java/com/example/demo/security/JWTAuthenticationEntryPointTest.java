a1cf9d64180af07d6b329da954944932749cfffa
Akhilesh Khapangee <akhapangee@gmail.com>
2023-01-30 16:50:06 -0600

    initial commit

---
+++ starter_code/src/test/java/com/example/demo/security/JWTAuthenticationEntryPointTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JWTAuthenticationEntryPointTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void givenAuthRequestOnPrivateService_shouldSucceedWith200() throws Exception {
+        mvc.perform(get("/api/user/id/1"))
+                .andExpect(status().isUnauthorized());
+    }
+
+}
